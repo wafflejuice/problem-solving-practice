5ce9c5e1920b33cb48c231c5a4136de4c9629cfb
wafflejuice <wafflejuice96@gmail.com>
2022-04-08 22:53:06 +0900

    faster version

--- leetcode/12. Integer to Roman.py
+++ leetcode/12. Integer to Roman.py
@@ -1,21 +1,29 @@
+from collections import deque
+
 class Solution:
     def intToRoman(self, num: int) -> str:
-        dividers=[1000,500,100,50,10,5,1]
-        symbols=['M','D','C','L','X','V','I']
-        roman=''
+        dividers=(1000,500,100,50,10,5,1)
+        symbols=('M','D','C','L','X','V','I')
+        
+        roman=deque()
         for di in range(0, len(dividers), 2):
             div,num=divmod(num, dividers[di])
             if div>0:
                 if div<4:
-                    roman+=symbols[di]*div
+                    for i in range(div):
+                        roman.append(symbols[di])
                 elif div==4:
-                    roman += symbols[di]+symbols[di-1]
+                    roman.append(symbols[di])
+                    roman.append(symbols[di-1])
                 elif div<9:
-                    roman+=symbols[di-1]+symbols[di]*(div-5)
+                    roman.append(symbols[di-1])
+                    for i in range(div-5):
+                        roman.append(symbols[di])
                 elif div==9:
-                    roman += symbols[di]+symbols[di-2]
+                    roman.append(symbols[di])
+                    roman.append(symbols[di-2])
                 
-        return roman
+        return ''.join(roman)
     
 print(Solution().intToRoman(3))  # "III"
 print(Solution().intToRoman(58)) # "LVIII"
